6df1986e4e00207693dfac68dd96adccdb4acf4e
Liubov Alekseeva <Liubov.Alekseeva@wuerth-cs.com>
2022-11-07 19:06:05 +0100

    [web] - add basic container

---
+++ web/src/containers/CompaniesList/CompaniesList.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Stack, StackDivider } from "@chakra-ui/react";
+import { CompanyItem, FullscreenSpinner } from "../../components";
+import { useGetCompanies } from "../../graphql";
+
+export const CompaniesList: React.FC = () => {
+  const { companies, loading } = useGetCompanies();
+
+  if (loading) return <FullscreenSpinner/>;
+
+  return (
+    <Stack
+      m={4}
+      direction={["column"]}
+      divider={<StackDivider borderColor='gray.200' />}
+      spacing={4}
+      align="flex-start"
+    >
+      {companies.map(company => (
+        <CompanyItem
+          key={company.uuid}
+          companyId={company.uuid}
+          title={company.name}
+        />
+      ))}
+    </Stack>
+  );
+};
